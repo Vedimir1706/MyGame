cf32d97f04ae3895b32c0d7d6ef56903b4ffd8fc
Vedimir1706 <45330772+Vedimir1706@users.noreply.github.com>
2018-11-30 06:47:46 +0600

    Add files via upload

---
+++ MyGame/MyGame/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Создаём шаблон приложения, где подключаем модули
+namespace MyGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+            Game.Init(form);
+            form.Show();
+            Game.Draw();
+            Application.Run(form);
+        }
+    }
+}
